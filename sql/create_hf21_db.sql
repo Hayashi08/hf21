a326c15098c94943bea2d5ee96bcc4002456131b
Hayashi <shuntaro.hayashi0829@gmail.com>
2020-12-27 21:15:52 +0900

    database fix

--- sql/create_hf21_db.sql
+++ sql/create_hf21_db.sql
@@ -16,6 +16,7 @@ CREATE TABLE user_tbl (
 CREATE TABLE session_tbl (
 	session_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	user_id INTEGER UNSIGNED NOT NULL,
+	session_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	company_name VARCHAR(64) DEFAULT '',
 	company_stage VARCHAR(16) DEFAULT '',
 	PRIMARY KEY (session_id),
@@ -25,11 +26,14 @@ CREATE TABLE session_tbl (
 CREATE TABLE result_tbl (
 	result_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	session_id INTEGER UNSIGNED NOT NULL,
-	result_datetime DATETIME,
-	result_time TIME DEFAULT '00:00:00',
+	result_start_time TIME DEFAULT '00:00:00',
+	result_end_time TIME DEFAULT '00:00:00',
 	result_total_point TINYINT UNSIGNED DEFAULT 0,
 	result_wasoku SMALLINT UNSIGNED DEFAULT 0,
+	result_wasoku_judge VARCHAR(64) DEFAULT '',
 	result_wasoku_point TINYINT UNSIGNED DEFAULT 0,
+	result_posture TINYINT UNSIGNED DEFAULT 0,
+	result_posture_judge VARCHAR(64) DEFAULT '',
 	result_posture_point TINYINT UNSIGNED DEFAULT 0,
 	PRIMARY KEY (result_id),
 	FOREIGN KEY (session_id) REFERENCES session_tbl (session_id)
@@ -38,11 +42,11 @@ CREATE TABLE result_tbl (
 CREATE TABLE sentence_tbl (
 	sentence_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	result_id INTEGER UNSIGNED NOT NULL,
-	sentence_str VARCHAR(512),
-	sentence_len TINYINT UNSIGNED DEFAULT 0,
 	sentence_time TIME DEFAULT '00:00:00',
+	sentence_str VARCHAR(512),
 	sentence_wasoku TINYINT UNSIGNED DEFAULT 0,
-	sentence_datetime DATETIME,
+	sentence_judge VARCHAR(64) DEFAULT '',
+	sentence_point TINYINT UNSIGNED DEFAULT 0,
 	PRIMARY KEY (sentence_id),
 	FOREIGN KEY (result_id) REFERENCES result_tbl (result_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -50,9 +54,10 @@ CREATE TABLE sentence_tbl (
 CREATE TABLE image_tbl (
 	image_id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
 	result_id INTEGER UNSIGNED NOT NULL,
+	image_time TIME DEFAULT '00:00:00',
 	image_path VARCHAR(64) DEFAULT '',
-	image_judge CHAR(1) DEFAULT '0',
-	image_datetime DATETIME,
+	image_judge VARCHAR(64) DEFAULT '',
+	image_point TINYINT UNSIGNED DEFAULT 0,
 	PRIMARY KEY (image_id),
 	FOREIGN KEY (result_id) REFERENCES result_tbl (result_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
