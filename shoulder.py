a6fc033b9999826c8d48b15d48cb23764cf0e57c
hiratayuki <hirata.yuki4050@gmail.com>
2021-01-06 15:28:37 +0900

    修正

--- shoulder.py
+++ shoulder.py
@@ -127,6 +127,11 @@ class Shoulder(object):
         # 右半分の直線を検知する。
         if x1 > self.detect_area[4] or x2 > self.detect_area[4]:
             result1 = -1
+        # 中央の直線を排除。
+        if x1>self.detect_area[4] and x2<self.detect_area[4]:
+            result1 = 'false'
+        if x1<self.detect_area[4] and x2>self.detect_area[4]:
+            result1 = 'false'
         # 上部の直線を除く
         if y1<self.detect_area[0] or y2<self.detect_area[0]:
             result1 = "false"
@@ -170,30 +175,35 @@ class Shoulder(object):
                     cv2.line(self.color_image,(x1,y1),(x2,y2),(0,0,255),2) # 描画
                     aline = np.append(aline, a)
                     right_flag = 1
+            if right_flag== 1 and left_flag== 1:
+                break
 
         # 描画後の画像保存
         save_path = MyImage.save(self.color_image)
         MyImage.all_save(self.color_image, self.all_img_path, '04')
 
-        num1 = aline[0]+aline[1]
-        num2 = 1-(aline[0]*aline[1])
-        flag = 0
-        tan = (-num2+np.sqrt(np.power(num2, 2)+np.power(num1, 2)))/num1
-        deg = np.rad2deg(np.arctan(tan))
-        if(deg>0):
-            flag = 1
+        if not aline[1:2]:
+            result = 'エラー'
         else:
-            deg = -deg
-        
-        if(deg > 4):
-            if(flag == 0):
-                # result = '右に' + str(round(deg, 1)) + '傾いています' + '\n' + str(aline[0]) + '\n' + str(aline[1])
-                result = '右に' + str(round(deg)) + '度傾いています'
+            num1 = aline[0]+aline[1]
+            num2 = 1-(aline[0]*aline[1])
+            flag = 0
+            tan = (-num2+np.sqrt(np.power(num2, 2)+np.power(num1, 2)))/num1
+            deg = np.rad2deg(np.arctan(tan))
+            if(deg>0):
+                flag = 1
             else:
-                # result = '左に' + str(round(deg, 1)) + '傾いています' + '\n' + str(aline[0]) + '\n' + str(aline[1])
-                result = '左に' + str(round(deg)) + '度傾いています'
-        else:
-            # result = 'OK' + '\n' + str(aline[0]) + '\n' + str(aline[1])
-            result = 'OK'
+                deg = -deg
+            
+            if(deg > 4):
+                if(flag == 0):
+                    # result = '右に' + str(round(deg, 1)) + '傾いています' + '\n' + str(aline[0]) + '\n' + str(aline[1])
+                    result = '右に' + str(round(deg)) + '度傾いています'
+                else:
+                    # result = '左に' + str(round(deg, 1)) + '傾いています' + '\n' + str(aline[0]) + '\n' + str(aline[1])
+                    result = '左に' + str(round(deg)) + '度傾いています'
+            else:
+                # result = 'OK' + '\n' + str(aline[0]) + '\n' + str(aline[1])
+                result = 'OK'
         return result, save_path
         
